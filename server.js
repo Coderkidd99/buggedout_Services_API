34d8e315b37d278db43aa21772779e0c376628f9
Bekim Burimi <107232029+Coderkidd99@users.noreply.github.com>
2023-03-28 20:45:37 +0700

    updated cors

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const { Pool } = require('pg');
 const app = express();
 
 app.use(cors());
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 
